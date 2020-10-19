7ed0aa0bf290ce71d46eb1ec7e4015e1588149a5
davidvenegasr <venegasr.david@gmail.com>
2020-10-19 16:57:45 -0300

    agregado ejemplo de guia para gaby

---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+file = open("data.in","r+") 
+lines = file.readlines()
+print(lines)
+
+
+# Se te ocurre como separar la lista lines en toras lsitas mas chiquitas? por categoria 
+
+cocina = []
+frases = []
+iconico = []
+especialidades = []
+curiosidades = []
