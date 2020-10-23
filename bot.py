0c435c69a46136b47e30ac8e4536953a5f8a87f3
gabyross <gabyross@users.noreply.github.com>
2020-10-23 19:15:35 -0400

    funcionamiento del bot

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
-import ast
+# Importamos las librerías necesarias
+from telegram import Update
+from telegram.ext import CallbackContext, Updater, MessageHandler, CommandHandler, InlineQueryHandler, Filters
+import random, ast
 
-'''# Método que imprimirá por pantalla la información que reciba
-def listener(bot, update):
-    id = update.message.chat_id
-    mensaje = update.message.text
 
-    print("ID: " + str(id) + " MENSAJE: " + mensaje)'''
-
+# Establecemos el token
+updater = Updater('1243199877:AAE11yhQrE0CGjhY7AmjW_ikWQxr9a67ZBc')
 
 #variables globales
 list_cocina = []
@@ -14,7 +13,6 @@ list_frases = []
 list_iconico = []
 list_especialidades = []
 list_curiosidades = []
-songs
 
 # Permite cargar un dictionario de un archivo que recibe como input
 def cargar_dictionario(file_name):
@@ -25,85 +23,106 @@ def cargar_dictionario(file_name):
     return dictionary
 
 
-#------------MAIN PART STARTS HERE
-def main():
-    file = open("data.in","r+") 
-    lines = file.read().splitlines()
+#------------LISTAS PART STARTS HERE
 
-    #declaracion de las categorias
-    global list_cocina
-    global list_frases
-    global list_iconico
-    global list_especialidades
-    global list_curiosidades
-    global songs 
+file = open("data.in","r+") 
+lines = file.read().splitlines()
 
-    #Cargo el diccionario de canciones
-    songs = cargar_dictionario("diccionario_canciones.txt")
+#Cargo el diccionario de canciones
+songs = cargar_dictionario("diccionario_canciones.txt")
     
 
-    flag = ""
-    #vamos a usar un flag que nos indicara que hacer segun la seccion donde estemos
-    for line in lines:
-        ##seteamos el flag solo cuando empieza una categoria
-        if line == "cocina":
-            flag = "cocina"
-            continue
-        elif line == "especialidades":
-            flag = "especialidades"
-            continue
-        elif line == "iconico":
-            flag = "iconico"
-            continue
-        elif line == "frases":
-            flag = "frases"
-            continue
-        elif line == "curiosidades":
-            flag = "curiosidades"
-            continue
-        print(flag)
-        ##ahora dependiendo del flag agregamos las lineas
-        if flag == "cocina":
-            list_cocina.append(line)
-        elif flag == "especialidades":
-            list_especialidades.append(line)
-        elif flag == "iconico":
-            list_iconico.append(line)
-        elif flag == "frases":
-            list_frases.append(line)
-        elif flag == "curiosidades":
-            list_curiosidades.append(line)
-        #seguro hay formas mas lindas de hacerlo pero esta fue la primera que se me ocurrio
-    
-    #actualizar()
-    #------------MAIN PART ENDS HERE
-
-    #Esto es lo que ejecuta el main cuando uno hace python3 bot.py
-if __name__ == '__main__':
-    main()
+flag = ""
+#vamos a usar un flag que nos indicara que hacer segun la seccion donde estemos
+for line in lines:
+    ##seteamos el flag solo cuando empieza una categoria
+    if line == "cocina":
+        flag = "cocina"
+        continue
+    elif line == "especialidades":
+        flag = "especialidades"
+        continue
+    elif line == "iconico":
+        flag = "iconico"
+        continue
+    elif line == "frases":
+        flag = "frases"
+        continue
+    elif line == "curiosidades":
+        flag = "curiosidades"
+        continue
+    print(flag)
+    ##ahora dependiendo del flag agregamos las lineas
+    if flag == "cocina":
+        list_cocina.append(line)
+    elif flag == "especialidades":
+        list_especialidades.append(line)
+    elif flag == "iconico":
+        list_iconico.append(line)
+    elif flag == "frases":
+        list_frases.append(line)
+    elif flag == "curiosidades":
+        list_curiosidades.append(line)
+#------------LISTAS PART ENDS HERE
 
 ##Primero se ejecuta el main, luego la parte del bot que hace polling, porque sino
 ##nunca se cargan las variables que quieres.
 
-'''def actualizar():
-    #creamos el Updater, objeto que se encargará de mandarnos las peticiones del bot
-    updater = Updater('1243199877:AAE11yhQrE0CGjhY7AmjW_ikWQxr9a67ZBc')
+## Inicio del establecimiento de las funciones de los comandos
+#------------START PART STARTS HERE
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hola {update.effective_user.first_name}, escribe "/" para ver los comandos disponibles')
+updater.dispatcher.add_handler(CommandHandler('start', start))
+#------------START PART ENDS HERE
 
-    #registramos los comandos del bot y su funcionalidad
-    dispatcher = updater.dispatcher
 
-    #registramos el método que hemos definido antes como listener para que muestre la información de cada mensaje
-    listener_handler = MessageHandler(Filters.text, listener)
-    dispatcher.add_handler(listener_handler)
+#------------COCINA PART STARTS HERE
+def cocina(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(random.choice(list_cocina))
+updater.dispatcher.add_handler(CommandHandler('cocina', cocina))
+#------------COCINA PART ENDS HERE
 
-    
-    ##Ahora registramos cada método a los comandos necesarios
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('cocina', cocina))
 
-    #comenzamos la ejecucion del bot a las peticiones
-    updater.start_polling()
-    updater.idle()
+#------------ESPECIALIDADES PART STARTS HERE
+def especialidades(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(random.choice(list_especialidades))
+updater.dispatcher.add_handler(CommandHandler('especialidades', especialidades))
+#------------ESPECIALIDADES PART ENDS HERE
 
-if __name__ == '__actualizar__':
-    main()'''
+
+#------------ICONICO PART STARTS HERE
+def iconico(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(random.choice(list_iconico))
+updater.dispatcher.add_handler(CommandHandler('iconico', iconico))
+#------------ICONICO PART ENDS HERE
+
+
+#------------FRASES PART STARTS HERE
+def frases(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(random.choice(list_frases))
+updater.dispatcher.add_handler(CommandHandler('frases', frases))
+#------------FRASES PART ENDS HERE
+
+
+#------------CURIOSIDADES PART STARTS HERE
+def curiosidades(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(random.choice(list_curiosidades))
+updater.dispatcher.add_handler(CommandHandler('curiosidades', curiosidades))
+#------------CURIOSIDADES PART ENDS HERE
+
+
+'''def get_url_dog():
+    contents = requests.get('https://random.dog/woof.json').json()  
+
+def get_url_cat():
+    contents = requests.get('').json()'''
+
+#------------CANCIONES PART STARTS HERE
+def canciones(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(random.choice(songs))
+updater.dispatcher.add_handler(CommandHandler('canciones', canciones))
+#------------CANCIONES PART ENDS HERE
+## Fin del establecimiento de las funciones de los comandos
+
+updater.start_polling()
+updater.idle()
